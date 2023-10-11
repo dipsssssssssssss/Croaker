801532be765f804da9ad5a567cf44fd63c5cf3c5
mdipshika <93993969+mdipshika@users.noreply.github.com>
2023-10-11 19:57:55 +0400

    task1

---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="header">
+        <img src="croaks.png" alt="Croaker Logo">
+        <h1>Croaker</h1>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    padding: 2px 2px;
+    color: rgb(0, 0, 0);
+    text-align: center;
+    background-color: #ece91f;
+    display: block;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+
+}
+
+.header img {
+    max-width: 100px;
+    height: auto;
+    
+}
+</style>
