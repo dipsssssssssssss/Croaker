9c7f136efa7d995c4e094af7add8484c24ab3055
mdipshika <93993969+mdipshika@users.noreply.github.com>
2023-11-01 00:39:13 +0400

    added more tests for email validation

--- src/components/__tests__/helpers.test.ts
+++ src/components/__tests__/helpers.test.ts
@@ -3,8 +3,30 @@ import { validateEmail } from "../helpers";
 
 
 // You need to write more tests for email (incorrect/correct)
+//incorrect ones
 describe('helpers - validateEmail', () => {
     it('fails if email is empty string', () => {
         expect(validateEmail('')).equal(false);
     });
 })
+it('returns false for an email without "@" symbol', () => {
+    expect(validateEmail('dipshikaemail.com')).toBe(false);
+
+});
+it('returns false for an email with spaces', () => {
+    expect(validateEmail('dipshika @email. com')).toBe(false);
+});
+
+it('returns false for an email without a local part', () => {
+    expect(validateEmail('@dipshika.com')).toBe(false);
+});
+
+//correct ones
+it('returns true for a valid email', () => {
+    expect(validateEmail('dipshika@email.com')).toBe(true);
+});
+
+it('returns true for a valid email with a single-character local part', () => {
+    expect(validateEmail('m@dipshika.com')).toBe(true);
+});
+
