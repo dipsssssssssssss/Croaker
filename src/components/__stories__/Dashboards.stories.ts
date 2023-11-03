479dd30f705b28edafbc78d97f095a7e3e793735
mdipshika <93993969+mdipshika@users.noreply.github.com>
2023-11-03 12:53:58 +0400

    created 3 stories for dashboard component

--- src/components/__stories__/Dashboards.stories.ts
+++ src/components/__stories__/Dashboards.stories.ts
@@ -1,29 +1,25 @@
-import { Meta, StoryObj } from "@storybook/vue3";
-import DashboardsVue from "../DashBoards.vue";
+import { Meta, StoryObj } from '@storybook/vue3';
+import Dashboard from '../DashBoards.vue';
 
 const meta = {
-    title: 'DashBoards',
-    component: DashboardsVue,
+  title: 'Dashboard',
+  component: Dashboard,
 } satisfies Meta;
 
-
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
-
 export const Default = () => ({
-  components: { DashboardsVue },
-  template: '<Dashboards />',
+  components: { Dashboard },
+  template: '<Dashboard />',
 });
 
 export const WithUserProfile = () => ({
-  components: { DashboardsVue },
-  template: '<Dashboards :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
+  components: { Dashboard },
+  template: '<Dashboard :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
 });
 
-
 export const WithNotifications = () => ({
-  components: { DashboardsVue },
-  template: '<Dashboards :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
+  components: { Dashboard },
+  template: '<Dashboard :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
 });
