c0e947e73f8119c837765d330f01ebe4d950af3f
mdipshika <93993969+mdipshika@users.noreply.github.com>
2023-11-03 12:40:21 +0400

    created 3 stories for dashboard

---
+++ src/components/__stories__/Dashboards.stories.ts
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/vue3";
+import DashboardsVue from "../DashBoards.vue";
+
+const meta = {
+    title: 'DashBoard',
+    component: DashboardsVue,
+} satisfies Meta;
+
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+
+export const Default = () => ({
+  components: { DashboardsVue },
+  template: '<Dashboards />',
+});
+
+export const WithUserProfile = () => ({
+  components: { DashboardsVue },
+  template: '<Dashboards :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
+});
+
+
+export const WithNotifications = () => ({
+  components: { DashboardsVue },
+  template: '<Dashboards :profileImageUrl="\'profile.jpg\'" :username="\'Dipshika\'" />',
+});
